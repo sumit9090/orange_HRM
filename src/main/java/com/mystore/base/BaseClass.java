e0550ab4e084058db22fce6555ddb232f645b8a7
sumit <sumit.kalra121@gmail.com>
2025-08-10 20:36:42 +0530

    first commit

--- src/main/java/com/mystore/base/BaseClass.java
+++ src/main/java/com/mystore/base/BaseClass.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterSuite;
@@ -76,7 +77,8 @@ public class BaseClass {
 		
 			
 			if (browserName.equalsIgnoreCase("Chrome")) {
-				
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless"); 
 				//driver=new ChromeDriver();
 				driver.set(new ChromeDriver());
 			
