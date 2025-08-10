86281574af434a8b6ac4c6f944df5f67a1561966
sumit <sumit.kalra121@gmail.com>
2025-08-10 20:21:44 +0530

    first commit

--- src/main/java/com/mystore/utility/ExtentManager.java
+++ src/main/java/com/mystore/utility/ExtentManager.java
@@ -7,33 +7,41 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 /**
  * @author Hitendra: ExtentManager class is used for Extent Report
  *  
- */
-public class ExtentManager {
-	
-	public static ExtentHtmlReporter htmlReporter;
-	public static ExtentReports extent;
-	public static ExtentTest test;
-	
-	public static void setExtent() {
-		//htmlReporter= new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output/ExtentReport/"+"MyReport_"+BaseClass.getCurrentTime()+".html");
-		htmlReporter= new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output/ExtentReport1/"+"MyReport.html");
-		htmlReporter.loadXMLConfig(System.getProperty("user.dir")+"/extent-config.xml");
-		//htmlReporter.config().setDocumentTitle("Automation Test Report");
-		//htmlReporter.config().setReportName("OrangeHRM Test Automation Report");
-		//htmlReporter.config().setTheme(Theme.DARK);
-		
-		extent = new ExtentReports();
-		extent.attachReporter(htmlReporter);
-		
-		extent.setSystemInfo("HostName", "MyHost");
-		extent.setSystemInfo("ProjectName", "MyAutomationStore");
-		extent.setSystemInfo("Tester", "Sumit");
-		extent.setSystemInfo("OS", "Win10");
-		extent.setSystemInfo("Browser", "Chrome");
-	}
-	public static void endReport() {
-		 if (extent != null) {
-	            extent.flush();
-	}
-	}
-}
+ */import com.aventstack.extentreports.ExtentReports;
+ import com.aventstack.extentreports.ExtentTest;
+ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+ public class ExtentManager {
+
+     public static ExtentHtmlReporter htmlReporter;
+     public static ExtentReports extent;
+     public static ExtentTest test;
+
+     public static void setExtent() {
+         if (extent == null) { // Prevent multiple initializations
+             String reportPath = System.getProperty("user.dir") + "/test-output/ExtentReport1/MyReport.html";
+             htmlReporter = new ExtentHtmlReporter(reportPath);
+
+             try {
+                 htmlReporter.loadXMLConfig(System.getProperty("user.dir") + "/extent-config.xml");
+             } catch (Exception e) {
+                 System.out.println("Warning: Could not load extent-config.xml. Using default config.");
+             }
+
+             extent = new ExtentReports();
+             extent.attachReporter(htmlReporter);
+
+             extent.setSystemInfo("HostName", "MyHost");
+             extent.setSystemInfo("ProjectName", "MyAutomationStore");
+             extent.setSystemInfo("Tester", "Sumit");
+             extent.setSystemInfo("OS", "Win10");
+             extent.setSystemInfo("Browser", "Chrome");
+         }
+     }
+
+     public static void endReport() {
+         if (extent != null) {
+             extent.flush();
+         }
+     }
+ }
